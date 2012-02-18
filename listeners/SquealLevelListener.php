4791fccfa13e936a6b03c52a59d6c5453d4e39ae
Cullen Walsh <cwalsh@fb.com>
2012-02-18 14:34:57 -0800

    Adding License headers

--- listeners/SquealLevelListener.php
+++ listeners/SquealLevelListener.php
@@ -1,5 +1,22 @@
 <?php
 /**
+ * This file is part of SquealIrcBot.
+ *
+ * SquealIrcBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SquealIrcBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SquealIrcBot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * This is just an example listener to show storing user state
  */
 class SquealLevelListener extends SquealCommandListener {
